7b65342710b97a2439e3a434a19cf5ebb1a6e5d4
Linda Gong <mengyugong5@gmail.com>
2018-02-26 16:54:33 -0500

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -14,19 +14,15 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage',{
-    from: 'Jhon',
-    text: 'See you then',
-    createAt: 123123
-  });
-
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
-  socket.on('createEmail',(newEmail) => {
-    console.log('createEmail', newEmail);
-  });
 
   socket.on('disconnect', () => {
     console.log('User was disconnected');
